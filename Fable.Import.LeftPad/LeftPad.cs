559b476943f9eff21cbd944bf98d8ee4355cd81c
Ilja Nosik <ilja.nosik@outlook.com>
2017-11-04 16:29:53 +0100

    Add overloaded method for optional argument

--- Fable.Import.LeftPad/LeftPad.cs
+++ Fable.Import.LeftPad/LeftPad.cs
@@ -6,6 +6,9 @@ namespace Fable.Import.LeftPad
     public class LeftPad
     {
         [Import("default", "left-pad")]
+        public extern static string leftPad(string str, int length);
+
+        [Import("default", "left-pad")]
         public extern static string leftPad(string str, int length, char ch = ' ');
     }
 }
--- Sample/App.fs
+++ Sample/App.fs
@@ -3,8 +3,15 @@ module Sample.App
 open Fable.Import.JS
 open Fable.Import.LeftPad
 
-let padded =
-    LeftPad.leftPad ("foo", 10)
-    |> fun s -> "<" + s + ">"
+let embrace s = "<" + s + ">"
 
-console.log padded
+let paddedFoo =
+    LeftPad.leftPad ("foo", 10)
+    |> embrace
+
+let paddedBar =
+    LeftPad.leftPad ("bar", 10, '_')
+    |> embrace
+
+console.log paddedFoo
+console.log paddedBar
